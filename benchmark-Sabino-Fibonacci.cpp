695d5c1927d0ee6f380714288de67447a09a1698
pspa <pspa@ug5c42.windows.cin.ufpe.br>
2026-09-01 11:28:40 -0300

    reduzi o tempo gasto e especifiquei n cout que foi em fibonacci

--- benchmark-Sabino-Fibonacci.cpp
+++ benchmark-Sabino-Fibonacci.cpp
@@ -11,7 +11,7 @@ int main() {
     auto inicio = std::chrono::steady_clock::now();
 
     //Calcular as 50 primeiras posições de fibonacci
-    for (int i = 0; i <= 50; i++) {
+    for (int i = 0; i <= 49; i++) {
         // O 'volatile' obriga a CPU a executar a conta em todos os ciclos
         volatile long long resultado = fibonacciRecursivo(i);
         std::cout << i << "º número " << resultado << std::endl;
@@ -22,9 +22,9 @@ int main() {
 
     //Calcula a duração total e converte para milissegundos
     auto duracao = std::chrono::duration_cast<std::chrono::milliseconds>(fim-inicio);
-    std::cout << "Tempo total gasto: " << duracao.count() << " milissegundos.\n";
+    std::cout << "Tempo total gasto de fibonacci: " << duracao.count() << " milissegundos.\n";
 
-    std::cout << "Tempo total gasto em segundos: " << std::chrono::duration_cast<std::chrono::duration<double>>(duracao).count() << "s.\n";
+    std::cout << "Tempo total gasto em segundos de fibonacci: " << std::chrono::duration_cast<std::chrono::duration<double>>(duracao).count() << "s.\n";
 
 
     return 0;
