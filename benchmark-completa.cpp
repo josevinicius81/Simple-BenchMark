e424aade62e50ac2d3c28625d2dcd1bd2c60e946
josevinicius81 <jvfvn@cin.ufpe.br>
2026-09-03 11:31:05 -0300

    COLOQUEI ALGUNS COUTS

--- benchmark-completa.cpp
+++ benchmark-completa.cpp
@@ -9,6 +9,9 @@
 
 int main() {
 
+    //MARCA O TEMPO EXATO
+    auto inicio_total = std::chrono::high_resolution_clock::now();
+
     int temp_algoritmo1 = algoritmo1();
     std :: cout << "O tempo para encontrar 7 números primos de Mersenne foi: " << temp_algoritmo1 << " ms" << std :: endl;
 
@@ -17,5 +20,9 @@ int main() {
 
     int temp_algoritmo3 = algoritmo3();
     std :: cout << "O tempo para encontrar 50 números da sequência de fibonacci foi de: " << temp_algoritmo3 << " ms" << std :: endl;
+
+    auto fim_total = std::chrono::high_resolution_clock::now();
+    auto duracao_total = std::chrono::duration_cast<std::chrono::milliseconds>(fim_total - inicio_total);
+    std :: cout << "O tempo total de execução foi: " << duracao_total.count() << " ms" << std :: endl;
 
 }
